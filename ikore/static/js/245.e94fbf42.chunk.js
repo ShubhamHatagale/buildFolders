b93d4fd1bc2add14d60cd30f41f46613ea7c908b
shubham <shubhamhatagale02@gmail.com>
2023-01-09 13:01:45 +0530

    9Jan2023(13:00)

---
+++ ikore/static/js/245.e94fbf42.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkikore_pilates=self.webpackChunkikore_pilates||[]).push([[245],{9245:function(e,r,s){s.r(r),s.d(r,{default:function(){return a}});s(2791),s(1518);var i=s(1906),n=s(184);var t=function(){return(0,n.jsxs)(n.Fragment,{children:[(0,n.jsx)("div",{className:"breadcrum",children:(0,n.jsx)("h1",{className:"breadcrum_title",children:"Certification"})}),(0,n.jsx)(i.Z,{})]})},a=(s(66),function(){return(0,n.jsx)("div",{className:"testimonilasvdo",children:(0,n.jsx)(t,{})})})}}]);
+//# sourceMappingURL=245.e94fbf42.chunk.js.map
